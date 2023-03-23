1327947f2e2319a885f21b1e315830bf7b6f09c3
morlinbrot <morlinbrot@mailbox.org>
2023-03-23 12:48:28 +0100

    Introduce variables and functions to reduce repetition; make settings optional

--- autolights.sh
+++ autolights.sh
@@ -1,30 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+IP_ADDRS=(
+  '192.168.178.51'
+  # Add lines here if you have multiple lights.
+)
+
+# To use pre-set values, see below.
+BRIGHTNESS=40
+TEMPERATURE=162
+
+switch() {
+  for addr in "${IP_ADDRS[@]}"
+  do
+    ADDR="http://${addr}:9123/elgato/lights"
+    echo "Switching on light at "$ADDR
+
+    SETTINGS='{"lights":[{"on":'"$1"'}]}'
+    # Use this to always turn on with the settings above.
+    # SETTINGS='{"lights":[{"brightness":'"${BRIGHTNESS}"',"temperature":'"${TEMPERATURE}"',"on":'"$1"'}],"numberOfLights":1}'
+
+    curl --location --request PUT $ADDR --header 'Content-Type: application/json' --data-raw $SETTINGS
+  done
+}
+
+switch_on() {
+  switch 1
+}
+
+switch_off() {
+  switch 0
+}
+
 
 # Begin looking at the system log via the steam sub-command. Using a --predicate and filtering by the correct and pull out the camera event 
 log stream --predicate 'subsystem == "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' | while read line; do
   
   # The camera start event has been caught and is set to 'On', turn the light on
   if echo "$line" | grep -q "= On"; then
-	
-	echo "Camera has been activated, turn on the light."
-		
-	# Light 1 - change IP to your first lights IP
-	curl --location --request PUT 'http://192.168.86.40:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
-
-	#Light 2 (remove if you only have 1 light), change IP to your second light
-	curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
-
+  	echo "Camera has been activated, turn on the light."
+    switch_on
   fi
 
   # If we catch a camera stop event, turn the light off.
   if echo "$line" | grep -q "= Off"; then
-	echo "Camera shut down, turn off the light."
-	
-	#Light 1 - set to off
-	curl --location --request PUT 'http://192.168.86.40:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
-
-	#Light 2 - set to off
-	curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
-
+    echo
+  	echo "Camera shut down, turn off the light."
+  	switch_off
   fi
 done
